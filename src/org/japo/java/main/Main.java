80dcccb0efed12f51bf613db9ec532477d7bd0a2
FAMILIA <FAMILIA@192.168.1.56>
2019-10-11 13:26:03 +0200

    :tada: Commit Inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 Sebastian Najarro Heredia - sebastian.najarro.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sebastian Najarro Heredia - sebastian.najarro.alum@iescamp.es
+ */
+public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+        
+        double altura;
+        
+        try {
+            
+            System.out.print("Mi altura es ................: ");
+            altura = SCN.nextDouble();
+            
+            System.out.printf(Locale.ENGLISH,"Mi altura es ................: %.2f m%n", altura);
+        } catch (Exception e) {
+            
+            System.out.println("ERROR: Entrada incorrecta");
+        } finally {
+            SCN.nextLine();
+        }
+        
+
+    }
+
+}
